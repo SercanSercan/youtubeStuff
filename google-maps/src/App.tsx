f9bdce2238876a42d2f4e281d3c524d7fbad44c1
sercan <sercan.leylek@fremtind.no>
2021-08-08 17:26:24 +0200

    Show calculated distance in km

--- google-maps/src/App.tsx
+++ google-maps/src/App.tsx
@@ -5,6 +5,7 @@ import {loadMapApi} from "./utils/GoogleMapsUtils";
 
 function App() {
     const [scriptLoaded, setScriptLoaded] = useState(false);
+    const [distanceKm, setDistanceKm] = useState<number>(-1);
 
     useEffect(() => {
         const googleMapScript = loadMapApi();
@@ -13,14 +14,20 @@ function App() {
         });
     }, []);
 
+    const renderDistanceSentence = () => {
+        return `Distance between selected marker and home address is ${distanceKm} km.`;
+    };
+
     return (
         <div className="App">
             {scriptLoaded && (
                 <Map
                   mapType={google.maps.MapTypeId.ROADMAP}
                   mapTypeControl={true}
+                  setDistanceKm={setDistanceKm}
                 />
             )}
+            {distanceKm > -1 && renderDistanceSentence()}
         </div>
     );
 }
