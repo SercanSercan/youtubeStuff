739253c69228aeee63c562f5d693c96a325e02f2
sercan <sercan.leylek@fremtind.no>
2021-08-14 16:24:42 +0200

    Add geometry library to google maps api

--- google-maps/src/utils/GoogleMapsUtils.ts
+++ google-maps/src/utils/GoogleMapsUtils.ts
@@ -1,5 +1,5 @@
 export const loadMapApi = () => {
-    const mapsURL = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places&language=no&region=NO&v=quarterly`;
+    const mapsURL = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=geometry,places&language=no&region=NO&v=quarterly`;
     const scripts = document.getElementsByTagName('script');
     // Go through existing script tags, and return google maps api tag when found.
     for (let i = 0; i < scripts.length; i++) {
